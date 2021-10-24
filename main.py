ee5954bb8367b1e24436449c3975b8520f8450e8
Slail Billicko Jean Pierre <sbillicko@gmail.com>
2021-10-24 14:03:57 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+def getNthFib(n):
+    """
+    The Fibonacci sequence is defined as follows: the first number of the sequence is the second number is 1 and the
+    nth! number is the sum of the (n - 13th and (n - 23th numbers. Write a function that takes in an integer n and
+    returns the nth Fibonacci number.
+    """
+    pass
